f71f8325e791e408ede568e4f98a6ed79ba67e81
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-01-02 00:50:57 -0500

    Feat: A connection was created with the database in MongoDB

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+from config.mongodb import mongo
 from routes.todo import todo
 
+load_dotenv()
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.getenv('CONNECTION_STRING')
+mongo.init_app(app)
 
 @app.route("/")
 def index():
